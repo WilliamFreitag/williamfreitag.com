bdd2acdaa5fd510adbb97cfce8e4bdf71503d730
William <william.h.freitag@gmail.com>
2020-06-10 10:36:31 -0400

    Implemented front-end

--- Node.js Web Server/Server.js
+++ Node.js Web Server/Server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/',(req,res)=>{
-  res.send("I have conquered the internet. Dab on them.");
+app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname+'/../Front End Content/index.html')));
+
+app.get('/redirect/githubs/this',(req,res)=>{
+	res.redirect(301,'https://google.com');
 });
 
-app.listen(81, ()=> console.log('listening on port 81.'));
+app.use((req,res)=>res.sendFile(path.resolve(__dirname+'/../Front End Content/'+req.url)));
+
+app.listen(81,()=> console.log('listening on port 81.'));
